86befee446c4534fdc1ab8d0a8c1ea1487184ffb
avalladaresm <avalladaresm@unitec.edu>
2021-03-16 05:33:34 -0600

    changed table styling

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,5 +1,4 @@
 import React, { useMemo } from "react"
-import Button from "./Button"
 import { useTable } from 'react-table'
 
 const Table = (props) => {
@@ -15,41 +14,49 @@ const Table = (props) => {
   } = useTable({ columns, data })
 
   return (
-    <table {...getTableProps()} className='table-auto max-w-full w-full mb-0 border border-blueGray-300'>
-      <thead style={{ backgroundColor: '#f8f9fa' }} >
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()} className='font-semibold'>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                className='whitespace-nowrap font-bold py-2 px-3 border-b border-gray-300 text-left'
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()} className='border-b border-gray-300'>
-              {row.cells.map(cell => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    className='whitespace-normal py-2 px-3'
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    <div className="flex flex-col px-4 items-center mb-5">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="align-middle inline-block sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border border-gray-300 sm:rounded-lg">
+            <table {...getTableProps()} className='table-auto max-w-full min-w-full divide-y divide-gray-200'>
+              <thead className='bg-gray-100' >
+                {headerGroups.map(headerGroup => (
+                  <tr {...headerGroup.getHeaderGroupProps()} className='font-semibold'>
+                    {headerGroup.headers.map(column => (
+                      <th
+                        {...column.getHeaderProps()}
+                        className='px-6 py-3 text-left text-sm text-gray-600 uppercase tracking-wider'
+                      >
+                        {column.render('Header')}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody {...getTableBodyProps()} className='bg-white divide-y divide-gray-200'>
+                {rows.map(row => {
+                  prepareRow(row)
+                  return (
+                    <tr {...row.getRowProps()} className='border-b border-gray-300'>
+                      {row.cells.map(cell => {
+                        return (
+                          <td
+                            {...cell.getCellProps()}
+                            className='px-6 py-3 whitespace-nowrap'
+                          >
+                            {cell.render('Cell')}
+                          </td>
+                        )
+                      })}
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
