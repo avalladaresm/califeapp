1b4727f7897cc7748cd0c799b78ab404414a308e
avalladaresm <avalladaresm@unitec.edu>
2021-03-18 05:26:18 -0600

    added quickquotes table to admin view

--- pages/admin/quickquotes/index.tsx
+++ pages/admin/quickquotes/index.tsx
@@ -1,17 +1,134 @@
+import { format } from "date-fns"
 import { GetServerSidePropsContext } from "next"
+import Link from 'next/link'
+import React from "react"
 import MainContainer from "../../../components/admin_navigation"
+import PageContent from "../../../components/PageContent"
+import Table from "../../../components/Table"
 import { useQuickQuotes } from "../../../services/QuickQuote"
 import { documentCookieJsonify } from "../../../utils"
 import { LoggedInUserCookieData } from "../../auth/AuthModel"
 
-const Dashboard = (props) => {
+const QuickQuote = (props) => {
   const { data } = useQuickQuotes(props?.cookies?.a_t)
+
+  const columns = [
+    {
+      id: 'id',
+      accessor: 'id',
+      Header: '',
+      Cell: (cell) => {
+        return (
+          <Link href={`/admin/quickquotes/${cell.row.values.id}`}>
+            <a className='text-blue-500 hover:underline active:text-blue-800'>Ver detalles</a>
+          </Link>
+        )
+      }
+    },
+    {
+      id: 'planName',
+      accessor: 'planName',
+      Header: 'Plan'
+    },
+    {
+      id: 'userName',
+      accessor: 'userName',
+      Header: 'Nombre de la cuenta'
+    },
+    {
+      id: 'installments',
+      accessor: 'installments',
+      Header: 'Periodo de pago',
+      Cell: (cell) => {
+        let installmentInWords = ''
+        switch (cell.row.values.installments) {
+          case 12:
+            installmentInWords = 'mensual'
+            break
+          case 6:
+            installmentInWords = 'bi-mensual'
+            break
+          case 4:
+            installmentInWords = 'trimestral'
+            break
+          case 2:
+            installmentInWords = 'semestral'
+            break
+          case 1:
+            installmentInWords = 'anual'
+            break
+        }
+        return <div>{installmentInWords}</div>
+      }
+    },
+    {
+      id: 'installmentPayment',
+      accessor: 'installmentPayment',
+      Header: 'Valor cuota',
+      Cell: (cell) => {
+        return (
+          <div>
+            {cell.row.values.installmentPayment.toLocaleString('en-US', { style: 'currency', currency: 'HNL', currencyDisplay: 'narrowSymbol', minimumFractionDigits: 2 })}
+          </div>
+        )
+      }
+    },
+    {
+      id: 'totalQuickQuote',
+      accessor: 'totalQuickQuote',
+      Header: 'Total',
+      Cell: (cell) => {
+        const { installmentPayment, downPayment } = cell.row.original
+        let installments = 0
+        let total = 0
+        switch (cell.row.values.installments) {
+          case 12:
+            installments = 11
+            break
+          case 6:
+            installments = 5
+            break
+          case 4:
+            installments = 3
+            break
+          case 2:
+            installments = 1
+            break
+          case 1:
+            installments = 0
+            break
+        }
+        total = (installments * installmentPayment) + downPayment
+        return (
+          <div>
+            {total.toLocaleString('en-US', { style: 'currency', currency: 'HNL', currencyDisplay: 'narrowSymbol', minimumFractionDigits: 2 })}
+          </div>
+        )
+      }
+    },
+    {
+      id: 'createdAt',
+      accessor: 'createdAt',
+      Header: 'Fecha de solicitud',
+      Cell: (cell) => {
+        return (
+          <div>
+            {format(new Date(cell.row.values.createdAt), 'PPPP pp')}
+          </div>
+        )
+      }
+    }
+  ]
 
   return (
     <MainContainer>
-      <div className='flex justify-center'>
-        Quick quotes!!!!!!!!!!
-      </div>
+      {
+        data ?
+          <PageContent title='Cotizaciones rápidas'>
+            <Table columns={columns} data={data} size='full' />
+          </PageContent> :
+          'Loading...'
+      }
     </MainContainer>
   )
 }
@@ -27,7 +144,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
       }
     }
   }
-  
+
   return {
     props: {
       cookies: {
@@ -38,4 +155,4 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   }
 }
 
-export default Dashboard
+export default QuickQuote
