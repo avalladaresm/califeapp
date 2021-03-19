0bcc9f745e45af37d100e6482e4d5670089ac0ea
avalladaresm <avalladaresm@unitec.edu>
2021-03-19 07:28:45 -0600

    added ssr function to /app route to retrieve plans data immediately

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,11 +1,42 @@
-import React from "react"
+import { GetServerSidePropsContext } from "next"
+import React, { Context } from "react"
 import MainContainer from '../../components/client_navigation'
-const MainApp = () => {
+import { documentCookieJsonify } from "../../utils"
+import { LoggedInUserCookieData } from "../auth/AuthModel"
+import { FetchAccountRolesOnly } from "../auth/AuthService"
+import { usePlans } from "../client/plans/PlansService"
+
+const MainApp = (props) => {
+  const { } = usePlans(props?.cookies?.a_t)
+
   return (
     <MainContainer>
       <div>Main App</div>
     </MainContainer>
   )
+}
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const parsedCookie: LoggedInUserCookieData = ctx.req.headers.cookie && documentCookieJsonify(ctx.req?.headers?.cookie)
+  if (!parsedCookie?.a_t) {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false
+      }
+    }
+  }
+
+  const accountRoles = await FetchAccountRolesOnly(parsedCookie)
+  return {
+    props: {
+      cookies: {
+        uid: parsedCookie.uid,
+        a_t: parsedCookie.a_t,
+        r: accountRoles
+      }
+    }
+  }
 }
 
 export default MainApp
